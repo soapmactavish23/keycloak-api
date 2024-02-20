5fa175c2170fe7ec7d2ace60552ddaed7ff5268d
Henrick <ricknogueira1231@gmail.com>
2024-02-20 18:37:56 -0300

    Authorities

--- src/main/java/com/hkprogrammer/api/config/SecurityConfig.java
+++ src/main/java/com/hkprogrammer/api/config/SecurityConfig.java
@@ -2,28 +2,38 @@ package com.hkprogrammer.api.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.RequestMatchers;
 
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
 public class SecurityConfig {
-    
-    // ROLE_USER
-    // ROLE_ADMIN
 
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http ) throws Exception {
+	// ROLE_USER
+	// ROLE_ADMIN
 
-        http
-        .csrf(csrf -> csrf.disable())
-        .oauth2ResourceServer(oauth2 -> oauth2
-        .jwt(jwt -> jwt.jwtAuthenticationConverter(new JWTConverter())));
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
+		http.csrf(csrf -> csrf.disable())
+				.oauth2ResourceServer(oauth2 -> oauth2.jwt(jwt -> jwt.jwtAuthenticationConverter(new JWTConverter())));
 
-        return http.build();
-    }
+		
+		http.authorizeRequests()
+			.requestMatchers(HttpMethod.GET, "/v3/api-docs/**", "/swagger-resources/**", "/swagger-ui/**", "/swagger-ui.html").permitAll()
+			.requestMatchers(HttpMethod.GET, "/pessoa/ativos").hasAnyRole("USER", "ADMIN")
+			.requestMatchers(HttpMethod.GET, "/pessoa").hasRole("ADMIN")
+			.requestMatchers(HttpMethod.DELETE, "/pessoa/**").hasRole("ADMIN")
+			.requestMatchers(HttpMethod.POST, "/pessoa").hasRole("ADMIN")
+			.requestMatchers(HttpMethod.PUT, "/pessoa").hasRole("ADMIN")
+			.requestMatchers(HttpMethod.PUT, "/pessoa/alterar-status/**").hasRole("ADMIN");
+		
+
+		return http.build();
+	}
 }
